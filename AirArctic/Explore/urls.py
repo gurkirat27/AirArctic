95a6a4a681c585b731dd729536e08dbab1be3fc5
Gurkirat <gurkirat2773@gmail.com>
2024-10-28 14:07:27 -0600

    Refactor Explore App

--- AirArctic/Explore/urls.py
+++ AirArctic/Explore/urls.py
@@ -5,24 +5,32 @@ from . import views
 
 urlpatterns = [
 
-   
+
+    #Page Display Endpoints
+    
     path('exploreTrip/',views.explore),
     path('departureFlights/',views.displayDepartureFlights, name='displayDepartureFlights'),
     path('returningFlights/',views.displayReturningFlights, name='displayReturningFlights'),
+    path('selectedDepartureFlight/',views.displaySelectedDepartureFlight, name='displaySelectedDepartureFlight'),
+    path('selectedReturningFlight/',views.displaySelectedReturningFlight, name='displaySelectedReturningFlight'),
+    path('itenary/',views.displayItenary, name='displayItenary'),
+    path('passangerPage/',views.displayPassengerPage, name='displayPassengerPage'),
+    path('reviewDetails/',views.reviewDetails, name='reviewDetails'),
+    path('bookingConfirmation/',views.bookingConfirmation, name='bookingConfirmation'),
+
+
+    #Processing/Redirecting Endpoints
+
+
     path('submitForm/',views.submitexploreform, name='submitexploreform'),
     path('submitdepartureForm/',views.submitdepartureform, name='submitdepartureform'),
     path('submitreturnForm/',views.submitreturnform, name='submitreturnform'),
     path('submitreturnselectedForm/',views.submitreturnselectedform, name='submitreturnselectedform'), 
-    path('selectedDepartureFlight/',views.displaySelectedDepartureFlight, name='displaySelectedDepartureFlight'),
-    path('selectedReturningFlight/',views.displaySelectedReturningFlight, name='displaySelectedReturningFlight'),
-    path('itenary/',views.displayItenary, name='displayItenary'),
     path('submititenaryform/',views.submititenaryform, name='submititenaryform'),
-    path('passangerPage/',views.displayPassengerPage, name='displayPassengerPage'),
     path('submititenaryPage/',views.submititenaryPage, name='submititenaryPage'),
-    path('reviewDetails/',views.reviewDetails, name='reviewDetails'),
     path('redirectPassangerPage/',views.redirectPassangerPage, name='redirectPassangerPage'),
     path('submitpassangerPage/',views.submitpassangerPage, name='submitpassangerPage'),
     path('submitreviewform/',views.submitreviewform, name='submitreviewform'),
-    path('bookingConfirmation/',views.bookingConfirmation, name='bookingConfirmation'),
+
     
 ]
--- AirArctic/Explore/views.py
+++ AirArctic/Explore/views.py
@@ -6,43 +6,57 @@ import random, string
 
 # Create your views here.
 
+## Page Display Views
+
+###Display Explore Page
 def explore(request):
 
    return render(request, 'exploreTrip.html')
 
+###Display Departure Flights Page
 def displayDepartureFlights(request):
 
    return render(request, 'displayDepartureFlights.html')
 
+###Display Returning Flights Page
 def displayReturningFlights(request):
 
    return render(request, 'displayReturningFlights.html')
 
+###Display Selected Departing Flight Page
 def displaySelectedDepartureFlight(request):
 
    return render(request, 'displaySelectedDepartingFlight.html')
 
+###Display Selected Returning Flight Page
 def displaySelectedReturningFlight(request):
 
    return render(request, 'displaySelectedReturningFlight.html')
 
+###Display Itenary Page
 def displayItenary(request):
 
    return render(request, 'displayItenary.html')
 
+###Display Passanger Form Page
 def displayPassengerPage(request):
 
    return render(request, 'displayPassengerPage.html')
 
+###Display Flight Review Page
 def reviewDetails(request):
 
    return render(request, 'reviewDetails.html')
 
+###Display Booking Page
 def bookingConfirmation(request):
 
    return render(request, 'bookingConfirmation.html')
 
+#----------------------------------------------------------------------------------------------------------------------------#
+## Data Processing Views
 
+###Once the Explore Form is submitted
 def submitexploreform(request):
   try:
     
@@ -54,11 +68,12 @@ def submitexploreform(request):
       departD = str(request.POST.get('startdate')) 
       returnD = str(request.POST.get('enddate')) 
       passangerD = str(request.POST.get('passanger'))
-
+      
+      #Storing this data in session for further use
       request.session['returnDate']=returnD
       request.session['passanger']=passangerD
+      request.session['tripType']=tripD
 
-      
       data = {
 
             'tripD':tripD,
@@ -69,19 +84,18 @@ def submitexploreform(request):
 
       }
 
-      
-      FINALDATA = data
       print(data)
       
+      #Redirected if trip selected is ONEWAY
       if returnD == "":
 
        url = "/api/departureFlights/?trip={}&from={}&to={}&departure={}".format(tripD,fromD,toD,departD)
        return redirect(url)   
        
+      #Redirected if trip selected is ROUNDTRIP
       else:
 
        url = "/api/departureFlights/?trip={}&from={}&to={}&departure={}&return={}".format(tripD,fromD,toD,departD,returnD)
-       
        return redirect(url)
       
     return tripD
@@ -90,8 +104,8 @@ def submitexploreform(request):
    pass
 
 
-  
-# check if its been used or not
+#----------------------------------------------------------#
+###Once the suitable departing flight is selected
 def submitdepartureform(request):
       
 
@@ -99,29 +113,31 @@ def submitdepartureform(request):
        
 
        selectedFlightId = request.GET.get("flight_id")
-       print("Slected flight id id = {}".format(selectedFlightId))
+       print("Slected flight id is = {}".format(selectedFlightId))
 
        url = "/api/selectedDepartureFlight/?flightId={}".format(selectedFlightId)
 
+       #Storing this data in session for further use
        request.session['dFlightId'] = selectedFlightId
-
 
        return redirect(url)
  
 
-
-
-      
+#----------------------------------------------------------#
+###Once the button to display Returning flights is clicked 
 def submitreturnform(request):
   
   try:
     if request.method=="POST":
 
+      #Getting To and From value from selected departing flight page(NOT RECOMMENDED)
+      #Change to receive it using session variable
       fromD = request.POST.get('fromForm')
       toD = request.POST.get('toForm')
+
+
       returnDate = request.session['returnDate']
       
-
       data = {
 
             'fromD':fromD,
@@ -136,7 +152,8 @@ def submitreturnform(request):
   except:
    pass
 
-# Check if its beenm used or not
+#----------------------------------------------------------#
+###Once the suitable returning flight is selected
 def submitreturnselectedform(request):
       
       
@@ -146,21 +163,24 @@ def submitreturnselectedform(request):
 
       url = "/api/selectedReturningFlight/?flightId={}".format(selectedReturnFlightId)
 
+      #Storing this data in session for further use
       request.session['rFlightId'] = selectedReturnFlightId
 
       return redirect(url)
 
 
+#----------------------------------------------------------#
+###Once the View Trip button is clicked
 def submititenaryform(request):
   try:
     
     if request.method=="POST":
 
-      #departingFlight = request.POST.get('departingFlightId')
-      #returningFlight = request.POST.get('returningFlightId')
+      #Retrieving session info
       returningFlight = request.session['rFlightId']
       departingFlight = request.session['dFlightId']
       passanger = request.session['passanger']
+      tripType = request.session['tripType']
       
 
       data = {
@@ -171,26 +191,30 @@ def submititenaryform(request):
       }
 
 
-      url = "/api/itenary/?trip={}&departingFlight={}&returningFlight={}&passanger={}".format("ROUNDTRIP",departingFlight,returningFlight,passanger)
+      url = "/api/itenary/?trip={}&departingFlight={}&returningFlight={}&passanger={}".format(tripType,departingFlight,returningFlight,passanger)
 
       return redirect(url)
   except:
    pass
   
+#----------------------------------------------------------#
+###Once the Continue on View Trip Page button is clicked  
 def submititenaryPage(request):
   try:
     
     if request.method=="POST":
-       
+      
+      #Getting data from Itenary page
       tripData = request.POST.get("tripStr")
       tripId = request.POST.get("trip_id")
       print(tripData)
       print(tripId)
 
+      #Storing Itenary data for further use
       request.session['tripInfo'] = tripData
       request.session['trip_id'] = tripId
 
-
+      #Retrieving Passanger Count
       passanger = request.session['passanger']
       print(passanger)
       
@@ -206,10 +230,11 @@ def submititenaryPage(request):
   except:
    pass
   
+#----------------------------------------------------------#
+###Once the trip data is successfully sent from Itenary page
 def redirectPassangerPage(request):
       
    try:  
-  
       passanger = request.session['passanger']
       print(passanger)
       
@@ -224,16 +249,18 @@ def redirectPassangerPage(request):
     pass
 
   
-  
+#----------------------------------------------------------#
+###Once the submit button is clicked on passanger page
 def submitpassangerPage(request):
 
   if request.method=="POST":
 
+   #Retrieving all info from passanger form
    fName = str(request.POST.get('firstName'))
    lName = str(request.POST.get('lastName')) 
    dob = str(request.POST.get('dob')) 
 
-
+   # POST Request to create passanger API
    url = "http://localhost:8000/api/passangers/"
    header = {
     "Content-Type":"application/json"
@@ -245,20 +272,15 @@ def submitpassangerPage(request):
    }
    result = requests.post(url,  data=json.dumps(payload), headers=header)
    result_json = result.json()
-   passangerId= result_json["id"]
 
+   #Retrieving passanger ID from response
+   passangerId= result_json["id"]
    print(passangerId)
 
-
+   # Storing passanger ID in session variable
    request.session['passanger_id'] = passangerId
- 
-
-
-
-   #Need a logic to fetch passangerId
-
-
-    
+   
+   #Retrieving Trip Info TO be sent in URL
    tripD = request.session['tripInfo']
 
    pData = {
@@ -273,21 +295,24 @@ def submitpassangerPage(request):
 
    return redirect(url)
   
+
+#----------------------------------------------------------#
+###Once the Complete Booking is clicked on Review page
 @csrf_exempt
 def submitreviewform(request):
   
-
   try:
-    
     if request.method=="POST":
-
+    
+    #---------CARD PROCESSING------------- 
+      #Retrieving card data from card form
       cardName = str(request.POST.get('cname'))
       cardNumber = str(request.POST.get('ccnum')) 
       expmonth = str(request.POST.get('expmonth')) 
       expyear = str(request.POST.get('expyear')) 
       cvv = str(request.POST.get('cvv')) 
-
       
+    # POST request to add card to database 
     url = "http://localhost:8000/api/cards/"
     header = {
       "Content-Type":"application/json"
@@ -300,31 +325,36 @@ def submitreviewform(request):
         "cvv": cvv
      }
     result = requests.post(url,  data=json.dumps(payload), headers=header)
-
     result_json = result.json()
+
+    #Retreiving card Id from API response
     cardId= result_json["id"]
 
+    #Storing card ID in session variable
     request.session['card_id'] = cardId
 
-   #Making call to POST Booking
+   #-------BOOKING PROCESSING-----
     
     #Fetch Trip Id
     trip_Id = request.session['trip_id']
-    print("TRIi={}".format(trip_Id)) 
   
     #Fetch Passanger Id
     passanger_Id = request.session['passanger_id']
 
     #Fetch Card Id
     card_Id = request.session['card_id']
-    
-    bookingRefernceNumber= ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+
+    #Creating Random Booking Refernce Number
+    brn= ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+
+    bookingRefernceNumber=brn
     tripId=trip_Id
     passangerId= passanger_Id
     cardId = card_Id
     isMember= False
     contactEmail= "gurkirat2773@gmail.com"
 
+    #POST request to add Booking to database
     url = "http://localhost:8000/api/bookings/"
     header = {
       "Content-Type":"application/json"
@@ -337,18 +367,16 @@ def submitreviewform(request):
         "isMember": isMember,
         "contactEmail": contactEmail
      }
+    
     result = requests.post(url,  data=json.dumps(payload), headers=header)
     result_json = result.json()
-    BookingId= result_json["bookingReferenceNumber"]
 
+    #Retrieving Booking Refernce Number from API response
+    bookingId= result_json["bookingReferenceNumber"]
 
-
-    url = "/api/bookingConfirmation/?bookingReferenceNumber={}".format(bookingRefernceNumber)
-
-
+    url = "/api/bookingConfirmation/?bookingReferenceNumber={}".format(bookingId)
 
     return redirect(url)
-
 
   except:
     pass
